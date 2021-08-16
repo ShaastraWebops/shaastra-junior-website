f9d2b7e3956e644b4852abd9c2fd898dfc784735
Mitesh <miteshbediya1999@gmail.com>
2021-08-17 00:31:46 +0530

    New

---
+++ src/competitions/Competitions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Competitions = () => {
+    return (
+        <div>
+            <h1>This is Competition page</h1>
+        </div>
+    )
+}
+
+export default Competitions
