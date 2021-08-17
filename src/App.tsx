63728a773f54c48cbea2a46e039e87623c3c2f84
SaiSrinivas321 <srinivas.32194@gmail.com>
2021-08-17 21:35:15 +0530

    Added chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
+import { css, Global } from "@emotion/react";
+import Loader from "./components/shared/Loader";
+
+const AppRoutes = React.lazy(() => import("./components/AppRoutes"));
+
+const GlobalStyles = css`
+.js-focus-visible :focus:not([data-focus-visible-added]) {
+     outline: none;
+     box-shadow: none;
+   }
+`;
+
+
+export const App = () => {
+  return (
+    <React.Suspense fallback={<Loader />}>
+      <ChakraProvider theme={theme}>
+        <Global styles={GlobalStyles} />
+        <AppRoutes />
+      </ChakraProvider>
+    </React.Suspense>
+  );
+  }
