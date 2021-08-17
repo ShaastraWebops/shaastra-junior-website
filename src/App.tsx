063ae6378ba57682bd52ed7c6a942d22ab8b797c
Mitesh <miteshbediya1999@gmail.com>
2021-08-17 20:23:27 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from '../src/homepage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
   );
 }
 
