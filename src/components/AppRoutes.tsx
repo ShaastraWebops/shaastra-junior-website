1de316159905280768b4b56c995df06fd95baa96
SaiSrinivas321 <srinivas.32194@gmail.com>
2021-08-18 10:28:04 +0530

    added app routes

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import {BrowserRouter as Router, Route} from "react-router-dom"
+import Competitions from "./pages/competitions/Competitions"
 import Home from "./pages/Home"
 
 interface Props {
@@ -10,6 +11,7 @@ const AppRoutes = (props: Props) => {
     return (
         <Router>
             <Route exact path="/" component={Home} />
+            <Route exact path="/competitions" component={Competitions} />
         </Router>
     )
 }
