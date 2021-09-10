553432e2032608355a67ef5889a766d9c4f60269
SaiSrinivas321 <srinivas.32194@gmail.com>
2021-09-10 16:05:14 +0530

    faq

--- src/components/pages/Events/EventPage.tsx
+++ src/components/pages/Events/EventPage.tsx
@@ -249,119 +249,154 @@ const EventPage = ()=>{
 
                 </Table>
              </Center>
-             <Container maxWidth="5xl">
-             <Center textAlign={"center"}>
-             <Heading size={'lg'} m={4}>FREQUENTLY ASKED QUESTIONS</Heading>
-             </Center>
-                {
-                    role === 'ADMIN' ? (
-                        <Flex >
-
-                        <FormControl m={2} width={"45%"}>
-                                <Input  name = "aquestion"
-                                placeholder = {'Question'}
-                                value={aquestion}
-                                onChange = {(event)=>handleAddFaq(event)}
-                                fontSize={'small'} p={2} borderColor={'#244f3b'}
-                               />
-                                </FormControl>
-                                <FormControl m={2} width={"45%"}>
-                                <Input name = "aanswer"
-                                placeholder = {'Answer'}
-                                value={aanswer}
-                                onChange = {(event)=>handleAddFaq(event)}
-                                fontSize={'small'} p={2} borderColor={'#244f3b'} />
-                                </FormControl>
-                                <Button color={'#244f3b'} variant="solid" border="2px solid"
-                                    borderColor = "#244f3b"size="sm" p={2} m={3}
-                                    onClick = {()=> handleAdd()}
-                                >Add FAQ</Button>
-                        </Flex>
-                    ) : null
-                    
-                }
-            
-             <Flex flexDirection={'column'} p={2}>
-                {
-                    event?.faqs.map((faq)=>{
-                        return(
-                            <Box m={4} key={faq.id}>
-                                <Flex>
-
-                                    <Box p={'5'} width={"100%"} borderTopRadius={"lg"} backgroundColor={"#467d63"}>
-                                       <Heading size = "md">{faq.question}</Heading>
-                                       {
-                                           faqId === faq.id ? (
-                                        <FormControl m={3}>
-                                            <Input 
-                                            onChange = {handleUpdateFaq} name ="uquestion"
-                                            value={uquestion} borderColor={'#244f3b'} placeholder="Question" color={"#244f3b"}/>
-                                        </FormControl>) :null
-                                       }
-                                    </Box>
-                                </Flex>
-                                <Flex>
-                                    <Box p={'5'} width={"100%"} backgroundColor={'#f4f7c6'} borderBottomRadius={"lg"} >
-                                    <Text fontWeight={"semibold"}>{faq.answer}</Text>
-                                    {
-                                           faqId === faq.id ? (
-                                            <FormControl m={3}>
-                                                <Input 
-                                                 onChange = {(event)=>handleUpdateFaq(event)}
-                                                 name = "uanswer"
-                                                 value={uanswer} borderColor={'#244f3b'} placeholder="Answer" color={"#244f3b"}/>
-                                            </FormControl>) :null
-                                       }
-                                  
-                                     {
-                                         faqId === faq.id ?
-                                         (
-                                            <Box>
-                                            <Button color={'#244f3b'} variant="solid" border="2px solid"
-                                            borderColor = "#244f3b"
-                                            size="sm" p={2} m={2}
-                                            onClick={() => handleedit(faq.id)}
-                                            float = {'right'}
-                                            >Edit FAQ</Button>
-                                            </Box>
-                                          )
-                                         :
-                                         (
-                                            role === "ADMIN" ?(<Box>
-                                                <Button color={'#244f3b'} variant="outline" border="2px solid"
-                                                borderColor = "#244f3b"
-                                                size="sm" p={2} m={2}
-                                                onClick={()=>{setFaqId(faq.id)}}
-                                                float = {'right'}
-                                                ><EditIcon m={2}/>Edit FAQ</Button>
-                                                <Button color={'#244f3b'} variant="outline" border="2px solid"
-                                                borderColor = "#244f3b"
-                                                size="sm" p={2} m={2}
-                                                onClick = {() =>{
-                                                    deleteFaq({variables :{
-                                                        deleteEventFaqEventFaqid : faq.id
-                                                    },
-                                                    refetchQueries : [{query:GETEVENT,variables:{ getEventEventId: id }}]
-                                                })
-                                                }}
-                                                float = {'right'}
-                                                ><DeleteIcon m={2}/>Delete FAQ</Button>
-                                                </Box>) : null
-                                            
-                                         )
-                                     }
-                                    
-    
-                                    </Box>
-                                   
-                                </Flex>
-                            </Box>
-
-                        )
-                    })
-                }
-             </Flex>
-             </Container>
+            {
+                event?.faqs.length! > 0 ? (
+                    <Container maxWidth="5xl">
+                    <Center textAlign={"center"}>
+                    <Heading size={'lg'} m={4}>FREQUENTLY ASKED QUESTIONS</Heading>
+                    </Center>
+                       {
+                           role === 'ADMIN' ? (
+                               <Flex >
+       
+                               <FormControl m={2} width={"45%"}>
+                                       <Input  name = "aquestion"
+                                       placeholder = {'Question'}
+                                       value={aquestion}
+                                       onChange = {(event)=>handleAddFaq(event)}
+                                       fontSize={'small'} p={2} borderColor={'#244f3b'}
+                                      />
+                                       </FormControl>
+                                       <FormControl m={2} width={"45%"}>
+                                       <Input name = "aanswer"
+                                       placeholder = {'Answer'}
+                                       value={aanswer}
+                                       onChange = {(event)=>handleAddFaq(event)}
+                                       fontSize={'small'} p={2} borderColor={'#244f3b'} />
+                                       </FormControl>
+                                       <Button color={'#244f3b'} variant="solid" border="2px solid"
+                                           borderColor = "#244f3b"size="sm" p={2} m={3}
+                                           onClick = {()=> handleAdd()}
+                                       >Add FAQ</Button>
+                               </Flex>
+                           ) : null
+                           
+                       }
+                   
+                    <Flex flexDirection={'column'} p={2}>
+                       {
+                           event?.faqs.map((faq)=>{
+                               return(
+                                   <Box m={4} key={faq.id}>
+                                       <Flex>
+       
+                                           <Box p={'5'} width={"100%"} borderTopRadius={"lg"} backgroundColor={"#467d63"}>
+                                              <Heading size = "md">{faq.question}</Heading>
+                                              {
+                                                  faqId === faq.id ? (
+                                               <FormControl m={3}>
+                                                   <Input 
+                                                   onChange = {handleUpdateFaq} name ="uquestion"
+                                                   value={uquestion} borderColor={'#244f3b'} placeholder="Question" color={"#244f3b"}/>
+                                               </FormControl>) :null
+                                              }
+                                           </Box>
+                                       </Flex>
+                                       <Flex>
+                                           <Box p={'5'} width={"100%"} backgroundColor={'#f4f7c6'} borderBottomRadius={"lg"} >
+                                           <Text fontWeight={"semibold"}>{faq.answer}</Text>
+                                           {
+                                                  faqId === faq.id ? (
+                                                   <FormControl m={3}>
+                                                       <Input 
+                                                        onChange = {(event)=>handleUpdateFaq(event)}
+                                                        name = "uanswer"
+                                                        value={uanswer} borderColor={'#244f3b'} placeholder="Answer" color={"#244f3b"}/>
+                                                   </FormControl>) :null
+                                              }
+                                         
+                                            {
+                                                faqId === faq.id ?
+                                                (
+                                                   <Box>
+                                                   <Button color={'#244f3b'} variant="solid" border="2px solid"
+                                                   borderColor = "#244f3b"
+                                                   size="sm" p={2} m={2}
+                                                   onClick={() => handleedit(faq.id)}
+                                                   float = {'right'}
+                                                   >Edit FAQ</Button>
+                                                   </Box>
+                                                 )
+                                                :
+                                                (
+                                                   role === "ADMIN" ?(<Box>
+                                                       <Button color={'#244f3b'} variant="outline" border="2px solid"
+                                                       borderColor = "#244f3b"
+                                                       size="sm" p={2} m={2}
+                                                       onClick={()=>{setFaqId(faq.id)}}
+                                                       float = {'right'}
+                                                       ><EditIcon m={2}/>Edit FAQ</Button>
+                                                       <Button color={'#244f3b'} variant="outline" border="2px solid"
+                                                       borderColor = "#244f3b"
+                                                       size="sm" p={2} m={2}
+                                                       onClick = {() =>{
+                                                           deleteFaq({variables :{
+                                                               deleteEventFaqEventFaqid : faq.id
+                                                           },
+                                                           refetchQueries : [{query:GETEVENT,variables:{ getEventEventId: id }}]
+                                                       })
+                                                       }}
+                                                       float = {'right'}
+                                                       ><DeleteIcon m={2}/>Delete FAQ</Button>
+                                                       </Box>) : null
+                                                   
+                                                )
+                                            }
+                                           
+           
+                                           </Box>
+                                          
+                                       </Flex>
+                                   </Box>
+       
+                               )
+                           })
+                       }
+                    </Flex>
+                    </Container>
+                ) : (
+                    <React.Fragment>
+                         {
+                           role === 'ADMIN' ? (
+                               <Flex >
+       
+                               <FormControl m={2} width={"45%"}>
+                                       <Input  name = "aquestion"
+                                       placeholder = {'Question'}
+                                       value={aquestion}
+                                       onChange = {(event)=>handleAddFaq(event)}
+                                       fontSize={'small'} p={2} borderColor={'#244f3b'}
+                                      />
+                                       </FormControl>
+                                       <FormControl m={2} width={"45%"}>
+                                       <Input name = "aanswer"
+                                       placeholder = {'Answer'}
+                                       value={aanswer}
+                                       onChange = {(event)=>handleAddFaq(event)}
+                                       fontSize={'small'} p={2} borderColor={'#244f3b'} />
+                                       </FormControl>
+                                       <Button color={'#244f3b'} variant="solid" border="2px solid"
+                                           borderColor = "#244f3b"size="sm" p={2} m={3}
+                                           onClick = {()=> handleAdd()}
+                                       >Add FAQ</Button>
+                               </Flex>
+                           ) : null
+                           
+                       }
+                    </React.Fragment>
+                )
+                
+            }
 
          </Flex>
            
