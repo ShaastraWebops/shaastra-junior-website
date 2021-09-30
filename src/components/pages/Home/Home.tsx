3b23c3982f386bb185234f5d4a37ffeeb87af8e1
Rohit Bhagat <rohitbhagat01010@gmail.com>
2021-09-30 18:19:46 +0530

    Added 4 spons images in homepage

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -10,36 +10,43 @@ import {
   Mahindra,
   Salesforces,
   Wells,
+  GFG,
+  wow_chess,
+  chess_com,
+  scholarships_in_india,
+  Amar_Chitra_Katha,
   exhibi,
   sj_logo,
   jmt,
   chess,
   spark,
-  brain
+  brain,
 } from "./Images";
 import Footer from "../../shared/Footer";
-import { useGetUsersCountQuery, useGetUsersDataCsvQuery, useTodaysHighlightsQuery } from "../../../types/generated/generated";
+import {
+  useGetUsersCountQuery,
+  useGetUsersDataCsvQuery,
+  useTodaysHighlightsQuery,
+} from "../../../types/generated/generated";
 import Loader from "../../shared/Loader";
 import { Link, useHistory } from "react-router-dom";
 import { Usercontext } from "../signinUp/Context";
 import RegisterNow from "../Events/RegisterNow";
-import {ReactComponent as Stats1} from "../../../images/home/3k.svg"
-import {ReactComponent as Stats2} from "../../../images/home/30.svg"
-import {ReactComponent as Stats3} from "../../../images/home/75.svg"
+import { ReactComponent as Stats1 } from "../../../images/home/3k.svg";
+import { ReactComponent as Stats2 } from "../../../images/home/30.svg";
+import { ReactComponent as Stats3 } from "../../../images/home/75.svg";
 import * as Showdown from "showdown";
 import "react-mde/lib/styles/css/react-mde-all.css";
-import parse from 'html-react-parser';
+import parse from "html-react-parser";
 import { EditIcon } from "@chakra-ui/icons";
 import fileDownload from "js-file-download";
-
 
 const converter = new Showdown.Converter({
   tables: true,
   simplifiedAutoLink: true,
   strikethrough: true,
-  tasklists: true
+  tasklists: true,
 });
-
 
 interface Props {}
 
@@ -58,30 +65,36 @@ const Home = (props: Props) => {
       <div className="home-landing-section">
         <div className="home-landing-head">Shaastra Juniors</div>
         <div className="home-landing-subhead">2021</div>
-        <div className="home-landing-subhead-date">24th - 26th Sep | 1st - 3rd Oct</div>
-        {
-          role === "USER" || role === "ADMIN" ?
-          <Link to="/workshops"><Button className="home-register-button">REGISTER</Button></Link>
-          : <Link to="/signup"><Button className="home-register-button">REGISTER</Button></Link>
-
-        }
-        {
-          role === "ADMIN" &&
+        <div className="home-landing-subhead-date">
+          24th - 26th Sep | 1st - 3rd Oct
+        </div>
+        {role === "USER" || role === "ADMIN" ? (
+          <Link to="/workshops">
+            <Button className="home-register-button">REGISTER</Button>
+          </Link>
+        ) : (
+          <Link to="/signup">
+            <Button className="home-register-button">REGISTER</Button>
+          </Link>
+        )}
+        {role === "ADMIN" && (
           <Flex alignItems={"center"} flexDirection={["column", "row"]}>
-            <Flex fontSize={"2xl"}>Total Number of Registration: {count?.getUsersCount}</Flex>
+            <Flex fontSize={"2xl"}>
+              Total Number of Registration: {count?.getUsersCount}
+            </Flex>
             <Button
               className="home-register-button"
               marginTop={"0px"}
               marginLeft={"15px"}
               onClick={() => {
-                fileDownload(csv?.getUsersDataCSV!,`sj_participants.csv`);
+                fileDownload(csv?.getUsersDataCSV!, `sj_participants.csv`);
               }}
             >
-            <EditIcon m={2} />
-            Download Participants Data
+              <EditIcon m={2} />
+              Download Participants Data
             </Button>
           </Flex>
-        }
+        )}
         {/* <div className="home-landing-subhead-init">An Initiative by Shaastra</div> */}
       </div>
 
@@ -95,8 +108,8 @@ const Home = (props: Props) => {
           </div>
           <div className="who-are-we-content">
             Shaastra Juniors, from Shaastra IIT Madras is the 1st ever
-            completely online Tech fest specially aimed at spreading
-            technical awareness among school students (Class 1 - 12). A fun filled weekend
+            completely online Tech fest specially aimed at spreading technical
+            awareness among school students (Class 1 - 12). A fun filled weekend
             filled with events, shows, lectures and workshops is sure to have
             each and every students yearning for more. Shaastra Juniors will be
             conducted online so each and every student can participate from the
@@ -132,9 +145,16 @@ const Home = (props: Props) => {
                   </div>
                   <div className="highlight-card-title">{dat.title}</div>
                   <div className="highlight-card-content">
-                  {parse(converter.makeHtml(dat.description.length > 100
-                      ? dat.description.substring(0, (window.innerWidth < 600 ? 175 : 235)) + "..."
-                      : dat.description))}
+                    {parse(
+                      converter.makeHtml(
+                        dat.description.length > 100
+                          ? dat.description.substring(
+                              0,
+                              window.innerWidth < 600 ? 175 : 235
+                            ) + "..."
+                          : dat.description
+                      )
+                    )}
                   </div>
                   <Flex
                     p={3}
@@ -231,9 +251,8 @@ const Home = (props: Props) => {
               arduous puzzles. Students had come to experience the bliss of
               solving mind-bending questions in Brain Squeeze. An entirely
               logical event which puts your reasoning and analytical skills to
-              the test with exceptionally tricky questions. Qualify the
-              prelims to battle it out with the brightest of minds in the
-              finals.
+              the test with exceptionally tricky questions. Qualify the prelims
+              to battle it out with the brightest of minds in the finals.
             </div>
           </div>
 
@@ -255,11 +274,11 @@ const Home = (props: Props) => {
           </div>
         </div>
         <div className="home-stats">
-          <Stats1 style={{flex :1}}  />
-          <Stats2 style={{flex :1}}  />
-          <Stats3 style={{flex :1}} />
+          <Stats1 style={{ flex: 1 }} />
+          <Stats2 style={{ flex: 1 }} />
+          <Stats3 style={{ flex: 1 }} />
         </div>
-        </div>
+      </div>
 
       <div>
         <div className="home-heads">Past Sponsors</div>
@@ -321,6 +340,46 @@ const Home = (props: Props) => {
               width="400px"
               p={8}
               alt="Wells"
+            />
+            <Image
+              style={{ objectFit: "contain" }}
+              src={GFG}
+              width="400px"
+              height="250px"
+              p={8}
+              alt="GFG"
+            />
+            <Image
+              style={{ objectFit: "contain" }}
+              src={wow_chess}
+              width="400px"
+              height="250px"
+              p={8}
+              alt="WOW Chess"
+            />
+            <Image
+              style={{ objectFit: "contain" }}
+              src={chess_com}
+              width="400px"
+              height="250px"
+              p={8}
+              alt="Chess.com"
+            />
+            <Image
+              style={{ objectFit: "contain" }}
+              src={scholarships_in_india}
+              width="400px"
+              height="250px"
+              p={8}
+              alt="Scholarships-in-india"
+            />
+            <Image
+              style={{ objectFit: "contain" }}
+              src={Amar_Chitra_Katha}
+              width="400px"
+              height="250px"
+              p={8}
+              alt="Amar-Chitra-Katha"
             />
           </Flex>
         </Box>
